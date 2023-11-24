a1abbf4933c9e874e813eb7bd008ff3726042f8e
vedasjad <96325125+vedasjad@users.noreply.github.com>
2023-11-24 19:43:47 +0530

    Assets added and Project Structure made

---
+++ lib/common/colors/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color redLight1 = Color(0xffd22f26);
+  static const Color red = Color(0xffd22f26);
+  static const Color redDark1 = Color(0xffb1060f);
+  static const Color grey = Color(0xff737373);
+  static const Color greyDark1 = Color(0xff323232);
+  static const Color greyDark2 = Color(0xff191919);
+  static const Color greyDark3 = Color(0xff010101);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+  static const Color greyLight3 = Color(0xffe6e6e6);
+  static const Color greyLight2 = Color(0xffcbcbcb);
+  static const Color greyLight1 = Color(0xffb2b2b2);
+  static const Color systemRed = Color(0xfffe0202);
+  static const Color systemBlue = Color(0xff4b6aea);
+}
---
+++ lib/features/stage/screens/stage_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StageScreen extends StatelessWidget {
+  const StageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
